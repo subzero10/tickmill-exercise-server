5071794068f86c73aff856e3dfb3cc13a1b74926
Pangratios Cosma <pagcosma@gmail.com>
2021-09-17 11:52:33 +0300

    first attempt to deploy

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,13 +1,14 @@
-import { SnakeNamingStrategy } from './src/snake-naming.strategy';
+import {SnakeNamingStrategy} from './src/snake-naming.strategy';
 
 module.exports = {
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  namingStrategy: new SnakeNamingStrategy(),
-  entities: ['src/modules/**/*.entity{.ts,.js}'],
-  migrations: ['src/migrations/*{.ts,.js}'],
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    namingStrategy: new SnakeNamingStrategy(),
+    entities: ['src/modules/**/*.entity{.ts,.js}'],
+    migrations: ['src/migrations/*{.ts,.js}'],
 };
